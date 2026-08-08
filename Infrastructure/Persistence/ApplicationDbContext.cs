f7deca4b1e1772cecc4b4f41efd693b118c49a03
Simply_ <icedeveloper247@gmail.com>
2026-08-08 23:16:14 +0100

    feat: add Identity classes and ApplicationDbContext with Identity support

---
+++ Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Identity;
+
+namespace Infrastructure.Persistence;
+
+public class ApplicationDbContext
+    : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+{
+    public ApplicationDbContext(
+        DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(ApplicationDbContext).Assembly);
+    }
+}
