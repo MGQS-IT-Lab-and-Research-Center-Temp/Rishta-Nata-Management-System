258fae371e02fb805f4e5c26aa9d682b8952fab3
zeebaby193 <zainabenitan422@gmail.com>
2026-08-08 21:12:12 +0100

    Add applictionstatus enum

---
+++ Domain/Enums/ApplicationStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Enums
+{
+    public enum ApplicationStatus
+    {
+        Draft = 10,
+
+        Submitted = 20,
+
+        Reviewed1Approved = 30,
+        Reviewed1Rejected = 40,
+
+        Reviewed2Approved = 50,
+        Reveiwed2Rejected = 60,
+
+        Reviewed3Approved = 70,
+        Reviewed3Rejected = 80
+    }
+}
