85be27e18fa8176165370dd4234829907ec0f48b
Simply_ <icedeveloper247@gmail.com>
2026-08-09 00:08:38 +0100

    Add services for database access

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,17 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Persistence;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
+// Add services for database access
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("DefaultConnection"))));
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
+
+
+var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.UseRouting();
