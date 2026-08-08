c12a306e781f7f25954ba4ecf543ac84b41ac50b
BABALOLA AMATULMUMIN <amatulmumin babalola07@gmail.com>
2026-08-08 21:14:22 +0100

    Add DI setup for Application and Infrastructure layers

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,7 +1,12 @@
+using Application;
+using Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
