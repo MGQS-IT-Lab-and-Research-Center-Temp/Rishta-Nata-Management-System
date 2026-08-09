d8737cc0388872be0fee4ec02bd48e4d1a502e0d
Simply_ <icedeveloper247@gmail.com>
2026-08-09 12:41:47 +0100

    Update ApplicationRole and ApplicationUser files

--- Infrastructure/Identity/ApplicationRole.cs
+++ Infrastructure/Identity/ApplicationRole.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Identity
 {
+    public string Description { get; set; } = default!;
     public class ApplicationRole : IdentityRole
     {
     }
--- Infrastructure/Identity/ApplicationUser.cs
+++ Infrastructure/Identity/ApplicationUser.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Infrastructure.Identity
 {
+    public bool IsActive { get; set; }
+    public bool MustChangePassword { get; set; }
+
     public class ApplicationUser : IdentityUser
     {
     }
