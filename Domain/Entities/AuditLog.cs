a49ea28a684493aaebcf8c72271a781dfbefc0ba
zeebaby193 <zainabenitan422@gmail.com>
2026-08-12 14:52:58 +0100

    I added AuditLog entity, configuration and DbContext

---
+++ Domain/Entities/AuditLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class AuditLog
+    {
+        public Guid Id { get; set; }
+        public Guid UserId { get; set; }
+        public string Action { get; set; } = string.Empty;
+        public string EntityName { get; set; } = string.Empty;
+        public Guid RecordId { get; set; }
+        public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+        public string ChangeDetails { get; set; } = string.Empty;
+    }
+}
