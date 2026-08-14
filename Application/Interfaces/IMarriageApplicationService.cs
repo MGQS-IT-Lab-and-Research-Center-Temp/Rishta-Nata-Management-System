cad659f65e34471b26d9cbdad590a3b244c62771
zeebaby193 <zainabenitan422@gmail.com>
2026-08-14 19:07:55 +0100

    Added IMarriageApplication Service and Interface

---
+++ Application/Interfaces/IMarriageApplicationService.cs
@@ -0,0 +1,12 @@
+﻿
+using Domain.Entities;
+
+namespace Application.Interfaces
+{
+    public interface IMarriageApplicationService
+    {
+        Task<MarriageApplication> CreateAsync(MarriageApplication marriageApplication);
+        Task<MarriageApplication?> GetByIdAsync(Guid id);
+        Task<List<MarriageApplication>> GetAllAsync();
+    }
+}
---
+++ Application/Services/MarriageApplicationService.cs
@@ -0,0 +1,39 @@
+﻿using Infrastructure.Persistence;
+using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Services
+{
+    public class MarriageApplicationService : IMarriageApplicationService
+    {
+        private readonly RishtanataDbContext _context;
+
+        public MarriageApplicationService(RishtanataDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<MarriageApplication> CreateAsync(
+            MarriageApplication marriageApplication)
+        {
+            _context.MarriageApplications.Add(marriageApplication);
+
+            await _context.SaveChangesAsync();
+
+            return marriageApplication;
+        }
+
+        public async Task<MarriageApplication?> GetByIdAsync(Guid id)
+        {
+            return await _context.MarriageApplications
+                .FindAsync(id);
+        }
+
+        public async Task<List<MarriageApplication>> GetAllAsync()
+        {
+            return await _context.MarriageApplications
+                .ToListAsync();
+        }
+    }
+}
