f473251bb865200a31e6c94755dc6f37151eefba
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2026-08-14 23:21:25 +0100

    fixed errors

--- Domain/Entities/NonAhmadiGuardian.cs
+++ Domain/Entities/NonAhmadiGuardian.cs
@@ -7,16 +7,16 @@ namespace Domain.Entities
     public class NonAhmadiGuardian
     {
         public int Id { get; set; }
-        public string? FirstName { get; set; }
-        public string LastName { get; set; }
-        public string OtherName
-        public string? PhoneNumber { get; set; }
-        public string? Address { get; set; }
-        public string Signature { get; set;}
+        public string FirstName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+        public string OtherName { get; set; } = default!;
+        public string PhoneNumber { get; set; } = default!;
+        public string Address { get; set; } = default!;
+        public string Signature { get; set; } = default!;
         public Guid MarriageApplicationFormId { get; set; }
-        public MarriageApplicationForm MarriageApplicationForm { get; set; }
+        public MarriageApplicationForm MarriageApplicationForm { get; set; } = default!;
 
-        public string? Religion { get; set; } 
+        public string? Religion { get; set; }
 
     }
 }
