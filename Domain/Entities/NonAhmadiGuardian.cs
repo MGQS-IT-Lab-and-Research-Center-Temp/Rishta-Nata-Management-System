3ab71df864f2da2910db117a17346d4fba4edc2a
Hafsat Dere <derehafsat24@gmail.com>
2026-08-14 18:26:44 +0100

    Add NonAhmadiGuardian entity to Domain.Entities

---
+++ Domain/Entities/NonAhmadiGuardian.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Entities
+{
+    public class NonAhmadiGuardian
+    {
+        public int Id { get; set; }
+        public string? FullNmae { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+
+        public string? Religion { get; set; } 
+
+    }
+}
