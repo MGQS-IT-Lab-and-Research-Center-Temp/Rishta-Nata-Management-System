e6c5733f7754b713e457728c9dc8b61ae2f24fa0
Ihsan-Ahmad Olamide Sodeko <ihsan.ahmadsodeko@gmail.com>
2026-08-14 21:31:38 +0100

    Update NonAhmadiGuardian.cs

--- Domain/Entities/NonAhmadiGuardian.cs
+++ Domain/Entities/NonAhmadiGuardian.cs
@@ -7,9 +7,14 @@ namespace Domain.Entities
     public class NonAhmadiGuardian
     {
         public int Id { get; set; }
-        public string? FullNmae { get; set; }
+        public string? FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OtherName
         public string? PhoneNumber { get; set; }
         public string? Address { get; set; }
+        public string Signature { get; set;}
+        public Guid MarriageApplicationFormId { get; set; }
+        public MarriageApplicationForm MarriageApplicationForm { get; set; }
 
         public string? Religion { get; set; } 
 
