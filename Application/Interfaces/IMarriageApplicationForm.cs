0a1e39e788853966b69fe2018c29229353a5dc5c
BABALOLA AMATULMUMIN <amatulmumin babalola07@gmail.com>
2026-08-15 12:55:34 +0100

    Add MarriageApplicationForm Service and Interface

---
+++ Application/Interfaces/IMarriageApplicationForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+    using Domain.Entities;
+
+    namespace Application.Interfaces;
+
+    public interface IMarriageApplicationFormService
+    {
+        Task<MarriageApplicationForm> CreateAsync(
+            MarriageApplicationForm application);
+
+        Task<MarriageApplicationForm?> GetByIdAsync(Guid id);
+
+        Task<bool> UpdateAsyn(MarriageApplicationForm application);
+    }
+
---
+++ Application/Services/MarriageApplicationFormService.cs
@@ -0,0 +1,50 @@
+﻿
+    using Application.Interfaces;
+    using Domain.Entities;
+    using Infrastructure.Persistence;
+    using Microsoft.EntityFrameworkCore;
+
+    namespace Application.Services;
+
+    public class MarriageApplicationFormService : IMarriageApplicationFormService
+    {
+        private readonly RishtanataDbContext _context;
+
+        public MarriageApplicationFormService(
+            RishtanataDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<MarriageApplicationForm> CreateAsync(
+            MarriageApplicationForm application)
+        {
+            _context.MarriageApplicationForms.Add(application);
+
+            await _context.SaveChangesAsync();
+
+            return application;
+        }
+
+        public async Task<MarriageApplicationForm?> GetByIdAsync(Guid id)
+        {
+            return await _context.MarriageApplicationForms
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+    public Task<bool> UpdateAsyn(MarriageApplicationForm application)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<bool> UpdateAsync(
+            MarriageApplicationForm application)
+        {
+            _context.MarriageApplicationForms.Update(application);
+
+            await _context.SaveChangesAsync();
+
+            return true;
+        }
+    }
+
