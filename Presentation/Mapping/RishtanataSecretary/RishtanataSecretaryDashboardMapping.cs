d3ebf3081775b8a8dd3e255f0b5e411c89d04376
zeebaby193 <zainabenitan422@gmail.com>
2026-08-18 15:11:20 +0100

    Mapping RishtanataDashboarddto and MarriedCoupledto into presentation

---
+++ Presentation/Mapping/RishtanataSecretary/RishtanataSecretaryDashboardMapping.cs
@@ -0,0 +1,21 @@
+﻿using Infrastructure.DTOs.RishtanataSecretaryDashboardDto;
+using Presentation.ViewModels;
+
+namespace Presentation.Mapping.RishtanataSecretary;
+
+public static class RishtanataSecretaryDashboardMapping
+{
+    public static RishtanataSecretaryDashboardViewModel ToViewModel(
+        RishtanataSecretaryDashboardDto dto)
+    {
+        return new RishtanataSecretaryDashboardViewModel
+        {
+            SecretaryName = dto.SecretaryName,
+            PendingApprovals = dto.PendingApprovals,
+            ApprovedApplications = dto.ApprovedApplications,
+            RejectedApplications = dto.RejectedApplications,
+            MarriedCouples = dto.MarriedCouples,
+            TotalMembers = dto.TotalMembers
+        };
+    }
+}
