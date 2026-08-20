12c92d14b47ff8d4598c3def13eb05c10f152373
Hafsat Dere <derehafsat24@gmail.com>
2026-08-20 09:54:26 +0100

    added roleassignmentservice in the program.cs

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -22,6 +22,8 @@ builder.Services.AddScoped<IRishtanataSecretaryService, RishtanataSecretaryServi
 builder.Services.AddScoped<IRoleAssignmentService, RoleAssignmentService>();
 
 
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
