6802cfca68cbcd72fdc1a8c8383210fc60fbfd1d
Simply_ <icedeveloper247@gmail.com>
2026-08-23 09:19:46 +0100

    chore(di): register form detail service

--- Presentation/Extensions/DependencyInjection.cs
+++ Presentation/Extensions/DependencyInjection.cs
@@ -33,6 +33,7 @@ public static class DependencyInjection
         services.AddScoped<IInvitationEmailService, InvitationEmailService>();
         services.AddScoped<IJamaatMemberService,JamaatMemberService>();
         services.AddScoped<IStageAuthorizationService, StageAuthorizationService>();
+        services.AddScoped<IMarriageApplicationFormDetailService, MarriageApplicationFormDetailService>();
         services.AddHttpClient<IGatewayHandler, GatewayHandler>();
         services.AddHttpContextAccessor();
 
