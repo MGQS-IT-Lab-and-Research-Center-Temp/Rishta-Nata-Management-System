3636574edcbe750406fd11a049ab4543452e9c89
Simply_ <icedeveloper247@gmail.com>
2026-08-26 05:35:54 +0100

    refactor(auth): replace email with ChandaNo in LoginViewModel

--- Presentation/ViewModels/LoginViewModel.cs
+++ Presentation/ViewModels/LoginViewModel.cs
@@ -4,15 +4,20 @@ namespace Presentation.ViewModels;
 
 public class LoginViewModel
 {
+    // Login with Email or Chanda Number?
+    /* 
     [Required(ErrorMessage = "Email is required.")]
     [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
     public string Email { get; set; } = string.Empty;
+    */
+
+    [Required(ErrorMessage = "Chanda Number is required.")]
+    [EmailAddress(ErrorMessage = "Please enter a valid chanda number")]
+    public string ChandaNo { get; set; } = string.Empty;
 
     [Required(ErrorMessage = "Password is required.")]
     [DataType(DataType.Password)]
     public string Password { get; set; } = string.Empty;
-
     public bool RememberMe { get; set; }
-
     public string? ReturnUrl { get; set; }
 }
